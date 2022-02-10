2cabe96e75e958b049158ef4f4d623a6479b0dd2
Firestar93 <MarkusHoffmann1993@web.de>
2022-02-10 14:48:30 +0100

    started modularizing spongEffects and vignette

--- R/fn_spongeffects_utility.R
+++ R/fn_spongeffects_utility.R
@@ -18,22 +18,40 @@ Convert_Gene_Names <- function(ceRNA_expression_data,
                                bioMart_gene_ensembl,
                                bioMart_gene_symbol_columns) {
 
-  ensembl <- biomaRt::useMart("ensembl", dataset=bioMart_gene_ensembl)
+  httr::set_config(httr::config(ssl_verifypeer = FALSE))
+  not_done=TRUE
 
-  Expr <-  Expr[!duplicated(Expr[,1]), ]
-  genes <- Expr[,1] %>% as.character()
+  ceRNA_expression_data <-  ceRNA_expression_data[!duplicated(ceRNA_expression_data[,1]), ]
+  genes <- ceRNA_expression_data[,1] %>% as.character()
 
-  GeneNames.df <- getBM(attributes=c('ensembl_gene_id',
-                                     'external_gene_name'),
-                        filters = bioMart_gene_symbol_columns,mart = ensembl,values = genes,
-                        useCache = FALSE)
+  G_list= data.frame()
 
-  Expr$Ensembl <- GeneNames.df[match(Expr[,1], GeneNames.df$external_gene_name), 1]
-  Expr <- Expr[!is.na(Expr$Ensembl), ]
+  while(not_done)
+  {
+    tryCatch({
+      ensembl <- biomaRt::useMart("ensembl", dataset=bioMart_gene_ensembl)
+      GeneNames.df <- getBM(attributes=c('ensembl_gene_id',
+                                         'external_gene_name'),
+                            filters = bioMart_gene_symbol_columns,mart = ensembl,values = genes,
+                            useCache = FALSE)
+      not_done=FALSE
+    }, warning = function(w) {
+      print("WARNING SECTION")
+      print(w)
+    }, error = function(e) {
+      print("ERROR SECTION")
+      print(e)
+    }, finally = {
+    })
+  }
 
-  rownames(Expr) <- Expr$Ensembl
 
-  return(dplyr::select(Expr[-1], -Ensembl))
+  ceRNA_expression_data$Ensembl <- GeneNames.df[match(ceRNA_expression_data[,1], GeneNames.df$external_gene_name), 1]
+  ceRNA_expression_data <- ceRNA_expression_data[!is.na(ceRNA_expression_data$Ensembl), ]
+
+  rownames(ceRNA_expression_data) <- ceRNA_expression_data$Ensembl
+
+  return(dplyr::select(ceRNA_expression_data[-1], -Ensembl))
 }
 
 #' Calculate z-scores
@@ -296,6 +314,12 @@ discretize<-function(v,
 
 #' Calculate enrichment scores
 #'
+#' @importFrom tidyverse
+#' @importFrom caret
+#' @importFrom foreach
+#' @importFrom doParallel
+#' @importFrom dplyr
+#'
 #' @param Expr.matrix ceRNA expression matrix
 #' @param modules Result of Define_Modules
 #' @param bin.size bin size (default: 100)
@@ -409,6 +433,231 @@ RF_Classifier <- function(Input.object,
   Output.rf <-list(Model = rf.model, ConfusionMatrix_training = Confusion.matrix)
   return(Output.rf)
 }
+
+#' prepare TCGA formats for spongEffects
+#'
+#' @importFrom tidyverse
+#' @importFrom caret
+#' @importFrom foreach
+#' @importFrom doParallel
+#' @importFrom dplyr
+#'
+#' @param tcga_cancer_symbol e.g., BRCA for breast cancer
+#' @param normal_ceRNA_expression_data normal ceRNA expression data
+#' (same structure as input for SPONGE)
+#' @param tumor_ceRNA_expression_data tumor ceRNA expression data
+#' (same structure as input for SPONGE)
+#' @param normal_metadata metadata for normal samples
+#' (TCGA format style, needs to include column: sampleID, PATIENT_ID)
+#' @param tumor_metadata metadata for tumor samples
+#' (TCGA format style, needs to include column: sampleID, PATIENT_ID)
+#' @param clinical_data clinical data for all patients
+#' (TCGA format style, needs to include column: PATIENT_ID, AJCC_PATHOLOGIC_TUMOR_STAGE)
+#' @param tumor_stages_of_interest array e.g.,
+#' c(STAGE I', 'STAGE IA', 'STAGE IB', 'STAGE II', 'STAGE IIA')
+#' @param subtypes_of_interest array e.g.,
+#' c("LumA",  "LumB",  "Her2",  "Basal", "Normal")
+#'
+#' @return list of prepared data. You can access it with list$objectname for
+#' further spongEffects steps
+prepare_tcga_for_spongEffects <- function(tcga_cancer_symbol,
+                                          normal_ceRNA_expression_data,
+                                          tumor_ceRNA_expression_data,
+                                          normal_metadata,
+                                          tumor_metadata,
+                                          clinical_data,
+                                          tumor_stages_of_interest,
+                                          subtypes_of_interest){
+
+  tcga_cancer_symbol=paste0(tcga_cancer_symbol,"_")
+
+  # Tidy and prepare input dataset
+  TCGA.expr.tumor <- tumor_ceRNA_expression_data %>% t() %>% as.data.frame()
+  TCGA.expr.normal <-normal_ceRNA_expression_data %>% t() %>% as.data.frame()
+  TCGA.meta.normal <- normal_metadata %>%
+    dplyr::filter(sampleID %in% colnames(TCGA.expr.normal))
+  TCGA.meta.tumor <- tumor_metadata %>%
+    dplyr::filter(sampleID %in% colnames(TCGA.expr.tumor))
+
+  TCGA.meta.tumor$PATIENT_ID <- substr(TCGA.meta.tumor$sampleID,1,nchar(TCGA.meta.tumor$sampleID)-3)
+
+  # Load metadata with grading information and keep only samples with StageI, II, III, and IV
+  # clean stage mess
+  MetaData_TCGA_Complete <- clinical_data %>%
+    dplyr::filter(!(SUBTYPE %in% c("")) & PATIENT_ID %in% TCGA.meta.tumor$PATIENT_ID) %>%
+    dplyr::filter(AJCC_PATHOLOGIC_TUMOR_STAGE %in% tumor_stages_of_interest) %>%
+    mutate(Grading = ifelse(AJCC_PATHOLOGIC_TUMOR_STAGE %in% c('STAGE I', 'STAGE IA', 'STAGE IB'), "Stage_I",
+                            ifelse(AJCC_PATHOLOGIC_TUMOR_STAGE %in% c('STAGE II', 'STAGE IIA', 'STAGE IIB'), "Stage_II",
+                                   ifelse(AJCC_PATHOLOGIC_TUMOR_STAGE %in% "STAGE IV", "Stage_IV", "Stage_III"))))
+
+  TCGA.meta.tumor <- TCGA.meta.tumor[match(MetaData_TCGA_Complete$PATIENT_ID, TCGA.meta.tumor$PATIENT_ID), ] %>%
+    left_join(MetaData_TCGA_Complete, .by = PATIENT_ID)
+  TCGA.meta.tumor$SUBTYPE <- gsub(tcga_cancer_symbol,"", TCGA.meta.tumor$SUBTYPE)
+  TCGA.meta.tumor$SUBTYPE <- factor(TCGA.meta.tumor$SUBTYPE, levels = subtypes_of_interest)
+
+  TCGA.expr.normal <- TCGA.expr.normal[, match(TCGA.meta.normal$sampleID, colnames(TCGA.expr.normal))]
+  TCGA.expr.tumor <- TCGA.expr.tumor[, match(TCGA.meta.tumor$sampleID, colnames(TCGA.expr.tumor))]
+
+  rownames(TCGA.expr.tumor) <- gsub("\\..*","",rownames(TCGA.expr.tumor))
+  rownames(TCGA.expr.normal) <- gsub("\\..*","",rownames(TCGA.expr.normal))
+
+  prep_TCGA_spongEffects <- list(TCGA.expr.tumor,TCGA.expr.normal,TCGA.meta.tumor,TCGA.meta.normal,MetaData_TCGA_Complete)
+  names(prep_TCGA_spongEffects) <- c("TCGA.expr.tumor","TCGA.expr.normal","TCGA.meta.tumor","TCGA.meta.normal","MetaData_TCGA_Complete")
+
+  return(prep_TCGA_spongEffects)
+}
+#' prepare METABRIC formats for spongEffects
+#'
+#' @importFrom tidyverse
+#' @importFrom caret
+#' @importFrom foreach
+#' @importFrom doParallel
+#' @importFrom dplyr
+#' @importFrom Biobase
+#'
+#' @param metabric_expression filepath to expression data in metabric format
+#' @param metabric_metadata filepath to metabric metadata in metabric format
+#' @param subtypes_of_interest array e.g.,
+#' c("LumA",  "LumB",  "Her2",  "Basal", "Normal")
+#' @param bioMart_gene_ensembl bioMart gene ensemble name
+#' (e.g., hsapiens_gene_ensembl).
+#' (See https://www.bioconductor.org/packages/release/bioc/vignettes/biomaRt/inst/doc/biomaRt.html)
+#' (default: hsapiens_gene_ensembl)
+#' @param bioMart_gene_symbol_columns bioMart dataset column for gene symbols
+#' (e.g. human: hgnc_symbol, mouse: mgi_symbol)
+#' (default: hgnc_symbol)
+#'
+#' @return list with metabric expression and metadata. You can access it with
+#' list$objectname for further spongEffects steps
+prepare_metabric_for_spongEffects <- function(metabric_expression,
+                                              metabric_metadata,
+                                              subtypes_of_interest,
+                                              bioMart_gene_ensembl = "hsapiens_gene_ensembl",
+                                              bioMart_gene_symbol_columns = "hgnc_symbol"){
+  METABRIC.expr <- read.delim(metabric_expression,
+                              header=T) %>% dplyr::select(-Entrez_Gene_Id)
+  METABRIC.expr <- METABRIC.expr[Biobase::isUnique(METABRIC.expr$Hugo_Symbol), ]
+
+  METABRIC.expr <- Convert_Gene_Names(METABRIC.expr,bioMart_gene_ensembl,bioMart_gene_symbol_columns)
+  colnames(METABRIC.expr) <- gsub("\\.", "-", colnames(METABRIC.expr))
+
+  METABRIC.meta <- read.delim(metabric_metadata, comment.char="#") %>%
+    filter(CLAUDIN_SUBTYPE %in% subtypes_of_interest & PATIENT_ID %in% colnames(METABRIC.expr))
+  METABRIC.meta$CLAUDIN_SUBTYPE <- factor(METABRIC.meta$CLAUDIN_SUBTYPE, levels = subtypes_of_interest)
+
+  METABRIC.expr <- METABRIC.expr[, match(METABRIC.meta$PATIENT_ID, colnames(METABRIC.expr))]
+
+  prep_metabric_spongEffects<-list(METABRIC.expr,METABRIC.meta)
+  names(prep_metabric_spongEffects) <- c("METABRIC.expr","METABRIC.meta")
+
+  return(prep_metabric_spongEffects)
+}
+
+#' prepare ceRNA network and network centralities from SPONGE / SPONGEdb
+#' for spongEffects
+#'
+#' @importFrom tidyverse
+#' @importFrom caret
+#' @importFrom foreach
+#' @importFrom doParallel
+#' @importFrom dplyr
+#'
+#' @param sponge_effects the ceRNA network downloaded as R object from SPONGEdb
+#' (Hoffmann et al., 2021) or created by SPONGE (List et al., 2019)
+#' usually ends with _sponge_results
+#' @param network_analysis the network analysis downloaded as R object
+#' from SPONGEdb (Hoffmann et al., 2021) or created by SPONGE
+#' (List et al., 2019) usually ends with _networkAnalysis
+#' if network_analysis is NA then the function only filters the ceRNA network
+#' @param mscor.threshold mscor threshold to be filtered (default: 0.1)
+#' @param padj.threshold adjusted p-value to be filtered (default: 0.01)
+#'
+#' @return list of filtered ceRNA network and network centrailies. You can
+#' access it with list$objectname for further spongEffects steps
+filter_ceRNA_network <- function(sponge_effects,
+                                 network_analysis = NA,
+                                 mscor.threshold = 0.1,
+                                 padj.threshold = 0.01){
+
+  #Filter SPONGE network for significant edges
+  Sponge.filtered <- sponge_effects %>%
+    filter_network(mscor.threshold =  .1, padj.threshold = .01)
+
+  # Calculate weighted centrality scores and add them to the ones present in SpongeDB
+  #cancerType.Centrality <- gsub(" ", "_", cancerType)
+
+  if(is.na(network_analysis))
+  {
+    sponge_network_centralites <- list(Sponge.filtered)
+    names(sponge_network_centralites) <- c("Sponge.filtered")
+  }
+  else
+  {
+    Node.Centrality <- network_analysis
+    Nodes <- Weighted_degree(Sponge.filtered, undirected = T, Alpha = 1)
+
+    Node.Centrality <- Node.Centrality %>%
+      mutate(Weighted_Degree = Nodes$Weighted_degree[match(Node.Centrality$gene, Nodes$Nodes)])
+    sponge_network_centralites <- list(Sponge.filtered,Node.Centrality)
+    names(sponge_network_centralites) <- c("Sponge.filtered","Node.Centrality")
+
+  }
+  return(sponge_network_centralites)
+
+}
+
+
+#' prepare ceRNA network and network centralities from SPONGE / SPONGEdb
+#' @importFrom tidyverse
+#' @importFrom caret
+#' @importFrom foreach
+#' @importFrom doParallel
+#' @importFrom dplyr
+#' @importFrom Biobase
+#' @importFrom biomaRt
+#'
+#' @param bioMart_gene_ensembl bioMart gene ensemble name
+#' (e.g., hsapiens_gene_ensembl).
+#' (See https://www.bioconductor.org/packages/release/bioc/vignettes/biomaRt/inst/doc/biomaRt.html)
+#' @param weighted_node_centrality output from filter_ceRNA_network()
+#' @param cutoff the top cutoff modules will be returned (default: 1000)
+#'
+#' @return top cutoff modules, with lncRNA as central genes
+get_lncRNA_modules <- function(bioMart_gene_ensembl,
+                               weighted_node_centrality,
+                               cutoff = 1000){
+  httr::set_config(httr::config(ssl_verifypeer = FALSE))
+  not_done=TRUE
+  while(not_done)
+  {
+    tryCatch({
+      ensembl=useMart("ensembl")
+      ensemblH = useDataset(bioMart_gene_ensembl,mart=ensembl)
+      Human.lncRNA = getBM(attributes=c("ensembl_gene_id","gene_biotype","description"), mart=ensemblH, useCache = FALSE) %>%
+        dplyr::filter(gene_biotype == "lncRNA")
+
+      not_done=FALSE
+    }, warning = function(w) {
+      print("WARNING SECTION")
+      print(w)
+    }, error = function(e) {
+      print("ERROR SECTION")
+      print(e)
+    }, finally = {
+    })
+  }
+
+
+  Node.Centrality.weighted <- weighted_node_centrality %>%
+    dplyr::filter(gene %in% Human.lncRNA$ensembl_gene_id) %>%
+    dplyr::arrange(desc(Weighted_Degree)) %>%
+    dplyr::slice(1:cutoff)
+
+  return(Node.Centrality.weighted)
+}
+
+
+
 
 
 
